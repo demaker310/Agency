33aa3904105612cacc730888f24548d7fc235f9e
ndidodley <ndidodley@gmail.com>
2021-05-02 20:28:53 +0100

    Agency is an agency website made by plain CSS, HTML, and JS
    First Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    nav: true,
+    center: true,
+    slideBy: 3,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true
+});
